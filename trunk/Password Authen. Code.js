69eb78b54f109efd1d468d81e35e125c56ca33d9
dhirajjadhao@gmail.com <dhirajjadhao@gmail.com@80e53325-ed52-24d3-81c8-974b80597bac>
2012-09-09 10:05:35 +0000

    cg

--- trunk/Password Authen. Code.js
+++ trunk/Password Authen. Code.js
@@ -1,8 +1,6 @@
-<script language="JavaScript">
 var password;
 var pass1="password";
 password=prompt('Enter Password To Log in to Arduino AutoHome',' ');
 if (password==pass1) 
 alert('WelCome To Arduino AutoHome');
 else { alert('Incorrect Password!'); }
-</script>
